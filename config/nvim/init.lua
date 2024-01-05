d193eed2cf121a508553e046fbf554861c7931b2
Presley Graham <presley.graham@protonmail.com>
2024-01-04 23:43:43 -0500

    more goodies

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -268,8 +268,10 @@ require("lazy").setup({
                         workspace = {
                             -- Make the server aware of Neovim runtime files
                             library = vim.api.nvim_get_runtime_file("", true),
+                            checkThirdParty = false,
                         },
                         diagnostics = { globals = { "vim" } },
+                        telemetry = { enable = false },
                     },
                 },
                 on_attach = on_attach,
